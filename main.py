4d6b95eda64b9f71c08be0a9cf10873614a7153a
thijsn <thijs@lastdrager.nl>
2024-04-22 19:45:03 +0200

    ALPHA 1

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 # main game written by thysl
 # images by GrafxKid on itch.io
 
-import pygame, time, random, math, array, time, numpy as np
-from scipy.io import wavfile
-from scipy.signal import butter, lfilter
+import pygame, time, random, math
 
 # CONSTANTS
 
@@ -50,6 +48,10 @@ LOGO_WIDTH, LOGO_HEIGHT = 275, 100
 LOGO_IMG = pygame.image.load('images/logo.png')
 LOGO_IMG = pygame.transform.scale(LOGO_IMG, (LOGO_WIDTH, LOGO_HEIGHT))
 
+TACHOMETER_WIDTH, TACHOMETER_HEIGHT = 300, 175
+TACHOMETER_IMG = pygame.image.load('images/tachometer.png')
+TACHOMETER_IMG = pygame.transform.scale(TACHOMETER_IMG, (TACHOMETER_WIDTH, TACHOMETER_HEIGHT))
+
 ROAD_WIDTH, ROAD_HEIGHT = 850, 600
 ROAD_IMG = pygame.image.load('images/road.png')
 ROAD_IMG = pygame.transform.scale(ROAD_IMG, (ROAD_WIDTH, ROAD_HEIGHT))
@@ -71,18 +73,19 @@ SHUTDOWN_SFX = pygame.mixer.Sound('sounds/ENGINE_SOUNDS/SHUTDOWN.wav')
 IDLE_SFX = pygame.mixer.Sound('sounds/ENGINE_SOUNDS/IDLE.wav')
 REDLINE_SFX = pygame.mixer.Sound('sounds/ENGINE_SOUNDS/REDLINE.wav')
 
+channel1 = pygame.mixer.Channel(2)
+
 # FUNCTIONS
-
-
 
 # CLASSES
 
 class engineClass:
     def __init__(self):
-        self.rpm = 3000
+        self.rpm = 0
         self.flywheel_mass = 1
         self.engine_friction = 10
-        self.playing_sound = None
+        self.playing_sound = self.rpm
+        self.ignition = False
     
     def get_power(self):
         try:
@@ -95,39 +98,59 @@ class engineClass:
                 self.power = 0
     
     def engine_sounds(self):
-        # numbered rpm
-        if type(self.rpm) == int or type(self.rpm) == float:
-            rpm = (round(self.rpm / 100)) * 100
-            print(rpm)
-            if not pygame.mixer.get_busy() or self.playing_sound != rpm:
-                pygame.mixer.stop()
-                REV_SFXS[rpm].play()
-                self.playing_sound = rpm
+        global keys
+        rpm = (round(self.rpm / 100)) * 100
 
-    # string rpm    
-        else:
-            if self.rpm == 'idle':
-                if not pygame.mixer.get_busy() or self.playing_sound != 'idle':
-                    pygame.mixer.stop()
-                    IDLE_SFX.play()
-                    self.playing_sound = 'idle'
-            elif self.rpm == 'redline':
-                if not pygame.mixer.get_busy() or self.playing_sound != 'redline':
-                    pygame.mixer.stop()
-                    REDLINE_SFX.play()
-                    self.playing_sound = 'redline'
+        # numbered rpm
+        if (type(self.rpm) == int or type(self.rpm) == float) and self.rpm < 9000 and (self.rpm > 2500 or keys[pygame.K_w]) and self.rpm > 1000:
+
+            # sound loop
+            if not channel1.get_busy() and self.playing_sound == rpm:
+                channel1.play(REV_SFXS[rpm])
+
+            # rpm change
+            if self.playing_sound != rpm:
+                channel1.stop()
+                channel1.play(REV_SFXS[rpm])
+                self.playing_sound = rpm
+        
+        # redline sfx
+        elif self.rpm > 9000:
+
+            # sound loop
+            if not channel1.get_busy() and rpm >= 9000:
+                channel1.play(REDLINE_SFX)
+            
+            # rpm change
+            if self.playing_sound < 9000 and rpm >= 9000:
+                channel1.stop()
+                channel1.play(REDLINE_SFX)
+                self.playing_sound = rpm
+        
+        # idle sfx
+        elif self.rpm > 2000 and self.rpm < 2500 and not keys[pygame.K_w]:
+
+            # sound loop
+            if not channel1.get_busy() and rpm > 2000 and rpm < 2500 and not keys[pygame.K_w]:
+                channel1.play(IDLE_SFX)
+            
+            # rpm change
+            if self.playing_sound != 420 and rpm > 2000 and rpm < 2500 and not keys[pygame.K_w]:
+                channel1.stop()
+                channel1.play(IDLE_SFX)
+                self.playing_sound = 420
     
     def accelerate(self):
-        if self.rpm < 9000:
+        if self.rpm < 9300:
             self.rpm += self.power / self.flywheel_mass
-        if self.rpm > 9000:
-            self.rpm = 9000
+        if self.rpm > 9300:
+            self.rpm -= 300
 
     def deccelerate(self):
-        if self.rpm > 1000:
+        if self.rpm > 2100:
             self.rpm -= self.engine_friction / self.flywheel_mass * engine.rpm / 750
-        elif self.rpm < 1000:
-            self.rpm = 1000
+        elif self.rpm < 2100 and self.rpm > 1000:
+            self.rpm += 200
 
 # CREATE CLASS INSTANCES
 
@@ -158,6 +181,7 @@ while run:
 
     engine.get_power()
     engine.engine_sounds()
+    print(f'rpm: {engine.rpm}, engine power: {engine.power} hp')    
 
     # drawing
 
@@ -165,10 +189,9 @@ while run:
     WIN.blit(LOGO_IMG, (950, 50))
     WIN.blit(ROAD_IMG, (20, 20))
     WIN.blit(BLUE_CAR_IMG, (150, 100))
+    WIN.blit(TACHOMETER_IMG, (1000, 500))
 
     pygame.display.flip()
 
     # rest
     clock.tick(FPS)
-
-    print(engine.rpm)
